fd8278c4aea555310d1e38c34e6ffaddbc7401a2
Junior-Stranner <JuniorStranner@gmail.com>
2025-08-06 21:17:48 -0300

    Usuario criado e controller com algumas validações implementadas

--- biblioteca-virtual-ada/src/main/java/br/com/nanotech/controller/UsuarioController.java
+++ biblioteca-virtual-ada/src/main/java/br/com/nanotech/controller/UsuarioController.java
@@ -1,5 +1,40 @@
 package br.com.nanotech.controller;
 
-public class UsuarioController {
+import br.com.nanotech.model.Usuario;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class UsuarioController {
+    private static final Logger logger = Logger.getLogger(UsuarioController.class.getName());
+    private final List<Usuario> usuarios = new ArrayList<>();
+    private Usuario usuarioLogado;
+
+    public void cadastrarUsuario(Usuario usuario) {
+        if (buscarPorEmail(usuario.getEmail()) != null) {
+            logger.warning("Tentativa de cadastro com email já existente: " + usuario.getEmail());
+            return;
+        }
+        this.usuarios.add(usuario);
+        System.out.println("Cadastro realizado com sucesso!");
+    }
+
+    public boolean login(String email, String senha) {
+        Usuario usuario = buscarPorEmail(email);
+        if (usuario != null && usuario.getSenha().equals(senha)) {
+            this.usuarioLogado = usuario;
+            return true;
+        }
+        return false;
+    }
+
+    private Usuario buscarPorEmail(String email) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getEmail().equalsIgnoreCase(email)) {
+                return usuario;
+            }
+        }
+        return null;
+    }
 }
